23a4fbcb8e37e7e03e9f1539c8aa40e449f06509
Miguel Palau Zarza <mpalauzarza@gmail.com>
2021-06-08 09:08:38 -0500

    moar scripting

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
+# APT
+sudo apt update
+sudo apt install fish zsh
+
 # Snap
 sudo apt install snapd
 
 # Vim Plug
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug
+
+# NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+nvm install node # "node" is an alias for the latest version
