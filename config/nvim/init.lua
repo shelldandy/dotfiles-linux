0e13a68796126308708de633839d617574fd0091
Miguel Palau Zarza <mpalauzarza@gmail.com>
2021-07-22 10:23:52 -0500

    lua

---
+++ config/nvim/init.lua
@@ -0,0 +1,13 @@
+-- Neovim Config Files by ShellDandy
+-- needs version 0.0.5 or higher
+
+vim.cmd('source ~/.config/nvim/download.vim')
+vim.cmd('source ~/.config/nvim/general.vim')
+vim.cmd('source ~/.config/nvim/functions.vim')
+vim.cmd('source ~/.config/nvim/coc.vim')
+vim.cmd('source ~/.config/nvim/plugins.vim')
+vim.cmd('source ~/.config/nvim/keys.vim')
+vim.cmd('source ~/.config/nvim/line.vim')
+vim.cmd('source ~/.config/nvim/nerdtree.vim')
+vim.cmd('source ~/.config/nvim/colors.vim')
+vim.cmd('source ~/.config/nvim/javascript.vim')
