7b3775a514d5a78134acb6aa53388605525f5f1f
Miguel Palau Zarza <mpalauzarza@gmail.com>
2023-02-03 12:16:26 -0600

    opaque dropdowns

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -370,7 +370,8 @@ local tasklist_buttons = gears.table.join(
             "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
             "Wpa_gui",
             "veromix",
-          "xtightvncviewer"},
+            "xtightvncviewer"
+          },
 
           -- Note that the name property shown in xprop might be set slightly after creation of the client
           -- and the name shown there might not match defined rules here.
