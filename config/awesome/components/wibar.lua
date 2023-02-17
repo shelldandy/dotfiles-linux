f9cfa609a2530de4f09ad656187b90d26167d14d
Miguel Palau Zarza <mpalauzarza@gmail.com>
2023-02-17 11:38:52 -0600

    semantics

--- config/awesome/components/wibar.lua
+++ config/awesome/components/wibar.lua
@@ -30,26 +30,26 @@ local mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.
 local mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
   menu = mymainmenu })
 
-bar.setup_bar = function (s)
+bar.setup_bar = function (screen)
   -- Create the wibox
-  s.mywibox = awful.wibar({ position = "top", screen = s })
+  screen.mywibox = awful.wibar({ position = "top", screen = screen })
 
   -- Add widgets to the wibox
-  s.mywibox:setup {
+  screen.mywibox:setup {
     layout = wibox.layout.align.horizontal,
     { -- Left widgets
       layout = wibox.layout.fixed.horizontal,
       mylauncher,
-      s.mytaglist,
-      s.mypromptbox,
+      screen.mytaglist,
+      screen.mypromptbox,
     },
-    s.mytasklist, -- Middle widget
+    screen.mytasklist, -- Middle widget
     { -- Right widgets
       layout = wibox.layout.fixed.horizontal,
       mykeyboardlayout,
       wibox.widget.systray(),
       mytextclock,
-      s.mylayoutbox,
+      screen.mylayoutbox,
     },
   }
 end
