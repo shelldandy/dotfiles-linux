b0ce4b983895a2f801f5e811e3fe3e624617a9a0
Miguel Palau Zarza <mpalauzarza@gmail.com>
2023-02-22 17:50:27 -0600

    lol

--- bin/autorun.sh
+++ bin/autorun.sh
@@ -11,7 +11,7 @@
 ##############################################
 
 # Set our displays
-run ""xrandr --output DP-2 --mode 3840x2160 --left-of DP-0 --output HDMI-0 --mode 3840x2160 --right-of DP-0
+xrandr --output DP-2 --mode 3840x2160 --left-of DP-0 --output HDMI-0 --mode 3840x2160 --right-of DP-0
 # Wallpapers
 nitrogen --restore
 # Screensaver
