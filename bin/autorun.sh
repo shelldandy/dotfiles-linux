8f13abda43706ea8152a23b08ab2c34c60ca2370
Miguel Palau Zarza <mpalauzarza@gmail.com>
2024-01-01 14:01:49 -0600

    passing validity check

--- bin/autorun.sh
+++ bin/autorun.sh
@@ -10,10 +10,10 @@
 # by @shelldandy
 ##############################################
 
-fkmd="--mode 3840x2160 --rate 60.00"
+FKM="--mode 3840x2160 --rate 60.00"
 
 # Set our displays
-xrandr --output HDMI-0 --primary $fkmd --output DP-0 $fkmd --left-of HDMI-0 --output DP-2 $fkmd --right-of HDMI-0
+xrandr --output HDMI-0 --primary $FKM --output DP-0 $FKM --left-of HDMI-0 --output DP-2 $FKM --right-of HDMI-0
 # Wallpapers
 nitrogen --restore
 # Screensaver
